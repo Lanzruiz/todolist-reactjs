b24b4b88168934a18eeb0393e39504c2b5d2e7eb
Lanz Ruiz <lanzruizdesigns@gmail.com>
2025-11-14 13:25:25 +0800

    setup workflows

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -7,10 +7,9 @@ const Landing = () => {
       <section className="landing">
         <div className="dark-overlay">
           <div className="landing-inner">
-            <h1 className="x-large">Developer Connector</h1>
+            <h1 className="x-large">Task Management System</h1>
             <p className="lead">
-              Create a developer profile/portfolio, share posts and get help
-              from other developers
+              Start your project with 100% management success!
             </p>
             <div className="buttons">
               <Link to="/register" className="btn btn-primary">
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -9,9 +9,6 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
-        <Link to="/profiles">Developers</Link>
-      </li>
-      <li>
         <Link to="/posts">Posts</Link>
       </li>
       <li>
@@ -32,9 +29,6 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <Link to="/profiles">Developers</Link>
-      </li>
-      <li>
         <Link to="/register">Register</Link>
       </li>
       <li>
@@ -47,7 +41,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
       <nav className="navbar bg-dark">
         <h1>
           <Link to="/">
-            <i className="fas fa-code"></i> DevConnector
+            <i className="fas fa-code"></i> TaskManage
           </Link>
         </h1>
         <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
