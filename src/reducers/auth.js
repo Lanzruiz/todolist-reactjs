979a299d466b3adf46ae00921de29fc998f4bd0f
Lanz Ruiz <lanzruizdesigns@gmail.com>
2025-11-14 16:31:43 +0800

    update

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -15,7 +15,7 @@ const initialState = {
   user: null,
 };
 
-export default function (state = initialState, action) {
+export default async function (state = initialState, action) {
   const { type, payload } = action;
   switch (type) {
     case USER_LOADED:
@@ -27,7 +27,7 @@ export default function (state = initialState, action) {
       };
     case REGISTER_SUCCESS:
     case LOGIN_SUCCESS:
-      localStorage.setItem("token", payload.token);
+      await localStorage.setItem("token", payload.token);
       return {
         ...state,
         ...payload,
@@ -39,7 +39,7 @@ export default function (state = initialState, action) {
     case AUTH_ERROR:
     case LOGIN_FAIL:
     case LOGOUT:
-      localStorage.removeItem("token");
+      await localStorage.removeItem("token");
       return { ...state, token: null, isAuthenticated: false, loading: false };
     default:
       return state;
